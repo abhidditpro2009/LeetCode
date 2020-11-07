6cfa237afcd86eb9ebf1bd3a74e094f9e40e0ee6
ABHIJEET J UPADHYE <abhidditpro2009@gmail.com>
2020-11-07 14:11:34 -0800

    6 ms

--- solutions/505. The Maze II.java
+++ solutions/505. The Maze II.java
@@ -1,43 +1,60 @@
-class Solution {
-    
-    enum ALGO {
-        BFS,
-        DFS,
-        DIJIKSTRA,
-        A_STAR
+    void dijikstra(int[][] maze, int x, int y, int dx, int dy, int[][] dist, boolean isAStar){
+        
+        PriorityQueue<int[]> queue ;
+        
+        if(isAStar)
+            queue = new PriorityQueue<>((a, b) -> (a[2] - b[2]));
+        else
+            queue = new PriorityQueue<>((a, b) -> Integer.compare(a[2], b[2]));
+        
+        queue.add(new int[]{x,y,1});
+        
+        
+        while(!queue.isEmpty()){
+            
+            int[] node = queue.poll();
+            int sx = node[0];
+            int sy = node[1];
+            int diff = node[2];
+​
+            if(sx == dx && sy == dy){
+                System.out.println(dist[sx][sy]);
+                return;
+            }
+​
+            for(int k=0;k<dirX.length;k++){
+​
+                int nsx = sx + dirX[k];
+                int nsy = sy + dirY[k];
+                int len = 1;
+                
+​
+                while(isInBounds(nsx,nsy,maze) && maze[nsx][nsy] != 1){
+                    nsx += dirX[k]; 
+                    nsy += dirY[k];
+                    len++;
+                }
+​
+                nsx -= dirX[k];
+                nsy -= dirY[k];
+                len--;
+​
+                if( len + diff < dist[nsx][nsy]){
+​
+                    dist[nsx][nsy] = len+ diff;
+                    queue.add(new int[]{nsx,nsy,dist[nsx][nsy]});
+                }
+​
+            }
+        
+        }
+​
     }
     
-    public int shortestDistance(int[][] maze, int[] start, int[] destination) {
+    void bfs(int[][] maze, int x, int y, int dx, int dy, int[][] dist){
         
-        int[][] dist = new int[maze.length][maze[0].length];
-        int len = 0;
+        Queue<int[]> queue = new LinkedList<>();
+    
+        queue.add(new int[]{x,y});
         
-        for (int i = 0; i < maze.length; i++) {
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-        }
-        
-        dist[start[0]][start[1]] = 1;
-        
-        ALGO algo = ALGO.BFS; 
-        
-        switch(algo){
-            
-            case DFS:
-                dfs(maze,start[0], start[1], destination[0], destination[1],dist,len+1);
-                return dist[destination[0]][destination[1]] == Integer.MAX_VALUE ? -1 : dist[destination[0]][destination[1]]-1;
-            
-            case BFS:
-                bfs(maze,start[0], start[1], destination[0], destination[1],dist);
-                return dist[destination[0]][destination[1]] == Integer.MAX_VALUE ? -1 : dist[destination[0]][destination[1]]-1;
-            
-            case A_STAR:
-                dijikstra(maze,start[0], start[1], destination[0], destination[1],dist,true);
-                return dist[destination[0]][destination[1]] == Integer.MAX_VALUE ? -1 : dist[destination[0]][destination[1]]-1;
-            
-            case DIJIKSTRA:
-                dijikstra(maze,start[0], start[1], destination[0], destination[1],dist,false);
-                return dist[destination[0]][destination[1]] == Integer.MAX_VALUE ? -1 : dist[destination[0]][destination[1]]-1;
-        
-            default:
-                dfs(maze,start[0], start[1], destination[0], destination[1],dist,len+1);
-                return dist[destination[0]][destination[1]] == Integer.MAX_VALUE ? -1 : dist[destination[0]][destination[1]]-1;
+        while(!queue.isEmpty()){
