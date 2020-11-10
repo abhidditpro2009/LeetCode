ebfbdd977eaa1526a98628524445891223b86449
ABHIJEET J UPADHYE <abhidditpro2009@gmail.com>
2020-11-10 02:44:08 -0800

    349 ms

--- solutions/1197. Minimum Knight Moves.java
+++ solutions/1197. Minimum Knight Moves.java
@@ -1,9 +1,31 @@
 class Solution {
+    
+    enum ALGO{
+        BFS,
+        A_STAR,
+        DIJIKSTRA,
+    }
+    
     public int minKnightMoves(int x, int y) {
+        
+        ALGO algo = ALGO.A_STAR;
         
         x = Math.abs(x);
         y = Math.abs(y);
-        return bfs(x,y);
+        
+        switch(algo){
+                
+            case BFS:
+                return bfs(x,y);
+            case A_STAR:
+                return A_Star(x,y);
+            case DIJIKSTRA:
+                return dijikstra(x,y);
+            default:
+                return bfs(x,y);
+        }
+        
+        
     }
     
     // We cannot use DFS in an infinite maze. It will get stuck in infinite loop.
@@ -14,26 +36,10 @@ class Solution {
         Set<String> set = new HashSet<>();
         Queue<int[]> queue = new LinkedList<>();
         
+        
         queue.add(new int[]{0,0});
         set.add("0_0");
         
         int[] dirX = {1,1,-1,-1,2,2,-2,-2};
         int[] dirY = {2,-2,2,-2,1,-1,1,-1};
         int count =0;
-        
-        while(!queue.isEmpty()){
-            
-            int size = queue.size();
-​
-            while(size-- > 0){
-            
-                int[] node = queue.poll();
-                int x = node[0];
-                int y = node[1];
-                
-                if(x == dx && y ==dy)
-                    return count;
-                
-                for(int k = 0; k < dirX.length;k++){
-​
-                    int nx = x + dirX[k];
